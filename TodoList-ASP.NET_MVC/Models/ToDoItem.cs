256945e765bfd337dfb25d172d393fb395acd88c
Angelicalindström <nutrition.angelica@gmail.com>
2025-03-26 11:57:11 +0100

    SetUp Base TodoItem + ToggleMethod status IsCompleted

---
+++ TodoList-ASP.NET_MVC/Models/ToDoItem.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoList_ASP.NET_MVC.Models
+{
+    public class ToDoItem
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public bool IsCompleted { get; set; }
+    }
+}
